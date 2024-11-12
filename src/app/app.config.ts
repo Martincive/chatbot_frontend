89497d44ef13e09b83f0b596fa57527356c71244
martincive <martincivetta@gmail.com>
2024-11-12 16:28:50 -0300

    Deploy Angular app to Azure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { ChatService } from './services/chatbot.service';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),
+    importProvidersFrom(FormsModule,  BrowserModule),
+    provideHttpClient(),
+    ChatService
+  ]
 };
